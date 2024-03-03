b51b30ce2e9f894dec06f420b04af1190fe65333
anuptachamo <anup.tachamo12@gmail.com>
2024-03-03 09:25:21 +0545

    JavaScript chapter10 [Main topic DOM Events], Covered topic => learn a concept about extracting form data [how form events work while clicking any submitting button]

---
+++ Chapter10/9_Extracting Form data/extractingFormData.js
@@ -0,0 +1,22 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  //event is default agrument
+  event.preventDefault(); //preventDefault help to stay in same page with submitting the form it won't indirect to /action page
+
+  /*let user = document.querySelector("#user");
+  let password = document.querySelector("#pass");*/
+
+  //   Alternative of code line no. 7 to 8
+  console.dir(form);
+  console.dir(form.elements);
+  let user = this.elements[0];
+  let password = this.elements[1];
+
+  console.dir(user);
+  console.dir(user.innerText); //In this case there will be not gonna print in innerText
+  console.dir(user.value); //In value the input text will be stored
+  console.dir(password.value); //In value the input text will be stored
+
+  alert(`Hi ${user.value}, your password is set to ${pass.value}`);
+});
