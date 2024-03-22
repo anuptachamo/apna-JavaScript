4deea15f507a75e5cb9bc87c59808002aa5b3b74
anuptachamo <anup.tachamo12@gmail.com>
2024-03-22 21:52:01 +0545

    Chapter12, covered topics: understanding about Ajax?

---
+++ Chapter12/5_Ajax/ajax.js
@@ -0,0 +1,5 @@
+/**
+ *  ? Ajax (Asynchronous JavaScript and XML)
+ *  => It is a process of calling API which through response came in data and this process happened as Asynchronously
+ *      
+ */
