e291ce4403583d0337d2d05e1855e8fc8236e7de
anuptachamo <anup.tachamo12@gmail.com>
2024-03-22 20:14:38 +0545

    Chapter12, covered topics: understand about Handling rejections using try and catch

--- Chapter12/3_async and await Example/asyncAndawaitExample.js
+++ Chapter12/3_async and await Example/asyncAndawaitExample.js
@@ -5,15 +5,29 @@ h2 = document.querySelector("h2");
 function changeColor(color, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+      let num = Math.floor(Math.random() * 5) + 1;
+      if (num > 3) {
+        reject("promise rejected");
+      }
       h2.style.color = color;
+      console.log(`color chnaged to ${color}!`);
       resolve("color changed!");
     }, delay);
   });
 }
 
 async function demo() {
-  await changeColor("red", 1000);
-  await changeColor("blue", 1000);
-  await changeColor("green", 1000);
-  await changeColor("yellow", 1000);
+  try {
+    await changeColor("red", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("green", 1000);
+    await changeColor("yellow", 1000);
+  } catch (err) {
+    console.log("some error occurs");
+    console.log(err);
+  }
+
+  let a = 5;
+  console.log(a);
+  console.log("new number =", a + 3);
 }
