a30736f9adcf501fcc353856c02c47da05f412b8
anuptachamo <anup.tachamo12@gmail.com>
2024-03-22 23:28:43 +0545

    Chapter12 completed, covered topics: Make first API request using promises, using Fetch with async await

---
+++ Chapter12/10_First API request example/first_API_request.js
@@ -0,0 +1,42 @@
+/**
+ *  ?Our First Request
+ *      using Fetch
+ *      fetch(url)
+ */
+
+let url = "https://catfact.ninja/fact";
+// fetch(url)
+//   .then((res) => {
+//     console.log(res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     console.log("data1 = ", data.fact);
+//     return fetch(url);
+//   })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((data2) => {
+//     console.log(data2.fact);
+//   })
+//   .catch((err) => {
+//     console.log("ERROR - ", err);
+//   });
+
+async function getFacts() {
+  try{
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data.fact);
+
+    let res2 = await fetch(url);
+    let data2 = await res2.json();
+    console.log(data2.fact);
+  } catch(e){
+    console.log("error -", e);
+  }
+
+  console.log("bye");
+}
