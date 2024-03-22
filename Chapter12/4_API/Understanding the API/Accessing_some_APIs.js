c29043100116f5fc7bdc9d7827b5f48f13683468
anuptachamo <anup.tachamo12@gmail.com>
2024-03-22 21:06:46 +0545

    Chapter12, covered topics: understanding What is a API and how its works?

---
+++ Chapter12/4_API/Understanding the API/Accessing_some_APIs.js
@@ -0,0 +1,6 @@
+/**
+ *  ? API (Some Random APIs)
+ *      => https://catfact.ninja/fact  (sends random cat facts)
+ *      => https://www.boredapi.com/api/activity (sends an activity to do when bored)
+ *      => https://dog.ceo/api/breeds/image/random (sends cute dog pictures)
+ */
---
+++ Chapter12/4_API/Understanding the API/api.js
@@ -0,0 +1,11 @@
+/**
+ *  ? What is API?
+ *      => Many people ask themselves, “What is an API?” API is the acronym for application programming interface — a software intermediary that allows two applications to talk to each other. APIs are an accessible way to extract and share data within and across organizations.
+
+APIs are all around us. Every time you use a rideshare app, send a mobile payment, or change the thermostat temperature from your phone, you’re using an API.
+
+* ? Understanding API(Application Programming Interface)
+        => API is like a waiter, first waiter take order from Me and waiter go to the Kitchen and place a order from the Kitchen to us.Now simply understand that ME is USER, WAITER is API and KITCHEN is a SERVER in which USER request API for data and API go  to the SERVER and SERVER give a data to API and API reponse to the USER.
+
+
+ */
