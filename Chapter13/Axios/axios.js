b0c5a320194116cdb82eac1b78fa6ef0edfc65e2
anuptachamo <anup.tachamo12@gmail.com>
2024-03-24 10:55:57 +0545

    Chapter13 started, covered topics: Understanding about axios using promises

---
+++ Chapter13/Axios/axios.js
@@ -0,0 +1,26 @@
+/**
+ *  ? Axios
+ *      Library to make HTTP requests
+ */
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async () => {
+  let fact = await getFacts();
+  console.log(fact);
+
+  let p = document.querySelector("#result");
+  p.innerText = fact;
+});
+
+let url = "https://catfact.ninja/fact";
+
+async function getFacts() {
+  try {
+    let res = await axios.get(url);
+    return res.data.fact;
+  } catch (e) {
+    console.log("error -", e);
+    return "No fact found";
+  }
+}
