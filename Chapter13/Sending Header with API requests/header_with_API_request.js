ca3bf2cc3ed5606eecd8d8bba9011258f61de4cf
anuptachamo <anup.tachamo12@gmail.com>
2024-03-25 20:14:26 +0545

    Chapter13, covered topics: Sending header with API requests

---
+++ Chapter13/Sending Header with API requests/header_with_API_request.js
@@ -0,0 +1,16 @@
+/**
+ *  ? Axios
+ *      Sending Headers
+ */
+
+const url = "https://icanhazdadjoke.com/";
+
+async function getJokes() {
+  try {
+    const config = { headers: { Accept: "application/json" } };
+    let res = await axios.get(url, config);
+    console.log(res.data);
+  } catch (err) {
+    console.log(err);
+  }
+}
